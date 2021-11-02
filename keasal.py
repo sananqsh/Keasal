f52086213f2fc9b8673c1d04c030f13dec1a1941
Sanan <74050360+sananqsh@users.noreply.github.com>
2021-11-03 01:38:17 +0330

    handled exceptions in repetition

--- keasal.py
+++ keasal.py
@@ -4,11 +4,10 @@ db = SQL("sqlite:///KeasalDB.db")
 
 position = 0
 
-positions = ["main", "language", "category", "word"]
+positions = ["database", "language", "category", "word"]
 
+# TODO: Handle exceptions: pos=0 & cmd > 2 || (pos in {1,2} & cmd > 4) || (pos == 3 & cmd in {1, 5, 6, ...})
 def guide(cmd, ref_id):
-    # TODO: Handle exceptions: pos=0 & cmd > 2 || (pos in {1,2} & cmd > 4) || (pos == 3 & cmd in {1, 5, 6, ...})
-
     global position
     if cmd == "0":
         position = 0
@@ -17,29 +16,25 @@ def guide(cmd, ref_id):
         print("2.About")
         return 0
     else:
-        cur_lvl = positions[position]
+        cur_lvl = positions[position]       
         if cmd == "1":
             if 0 < position:
-                new_id = fetch_id(cur_lvl)
-                if new_id == -1:
-                    # Exception
+                new_id = fetch_id(cur_lvl, ref_id)
+                if new_id == -1:    # Exception
                     return ref_id
                 ref_id = new_id
 
             position +=1
             cur_lvl = positions[position]
-
+            
             if position in range(1, 4):
                 print_elements(cur_lvl, ref_id)
 
-            # Uncomment to debug:
-            # print(f"ref_id: {ref_id}")
-            if position < 3:
-                next_lvl = positions[position+1]
-
             print("0.Go to main")
             if position in {1, 2}:
+                next_lvl = positions[position+1]
                 print(f"1.Manage in {next_lvl} level")
+
             print(f"2.Add new {cur_lvl}")
             print(f"3.Edit {cur_lvl}")
             print(f"4.Remove {cur_lvl}")
@@ -48,40 +43,55 @@ def guide(cmd, ref_id):
         else:
             if cmd == "2":
                 if position == 0:
-                    print("This program helps expand your vocabulary when learning a new language")
-                    print("It provides you three levels of: language, category and word")
-                    print("That`s right! you can store words in different languages and have them categorized!")
-                    print("The name of this project is 'Keasal', a Kurdish word meaning 'turtle',")
-                    print("which is a symbol of wisdom and patience. May you enjoy your journey ;)")
+                    print_about()
                     return ref_id
                 add(cur_lvl, ref_id)
             elif cmd == "3" or cmd == "4":
-                element_id = fetch_id(cur_lvl, "edit or remove")
-                if element_id == -1:
+                element_id = fetch_id(cur_lvl, ref_id, "edit or remove")
+                if element_id == -1:    # Exception
                     return ref_id
-                print(element_id)
                 if cmd == "3":
                     edit(cur_lvl, element_id)
                 if cmd == "4":
                     remove(cur_lvl, element_id)
             else:
                 print("Incorrect command!")
-                position = 0
-                reference_id = 0
+                # position = 0
                 return ref_id
 
+            if position != 0:
+                # print(cur_lvl)
+                # print(ref_id)
+                print_elements(cur_lvl, ref_id)
+
+            return ref_id
+
+# TODO: handle repeated categories
 def add(level, reference):
     new_name = input(f"Name for the new {level}: ")
     if level == "language":
         db.execute("INSERT INTO language (name) VALUES (?)", new_name)
     elif level == "category":
+        if is_repeated(new_name, level, reference):
+            return
         db.execute("INSERT INTO category (name, language_id) VALUES (?,?)", new_name, reference)
     elif level == "word":
+        if is_repeated(new_name, level, reference):
+            return
         meaning = input("Meaning for the new word: ")
         db.execute("INSERT INTO word (name, meaning, category_id) VALUES (?, ?, ?)", new_name, meaning, reference)
 
+def is_repeated(new_name, level, reference):
+    if level == "category":
+        repetition = db.execute("SELECT * FROM ? WHERE language_id=? AND name=?",level, reference , new_name)
+    else:
+        repetition = db.execute("SELECT * FROM ? WHERE category_id=? AND name=?",level, reference , new_name)
+    if len(repetition) != 0:
+        print(f"This {level} already exists!")
+        return
+
 def edit(level, element_id):
-    print(level + str(element_id))
+    # print(level + str(element_id))
     if level == "word":
         editting_col = input("What do you want to edit in this element(only name or meaning)? ")
         if editting_col not in {"name","meaning"}:
@@ -94,7 +104,7 @@ def edit(level, element_id):
         db.execute("UPDATE ? SET name=? where id=?;", level, updated_name, element_id)
 
 def remove(level, element_id):
-    db.execute("DELETE FROM ? where id = ?;", level, element_id)
+    db.execute("DELETE FROM ? where id = ?;", level, element_id)    
 
 def print_elements(level, reference):
     print("Current elements: { ")
@@ -114,21 +124,33 @@ def print_elements(level, reference):
             print(el["name"])
 
     print("}")
+    
+def fetch_id(level, reference, prompt="access"):
+    if level == "language":
+        selected = db.execute("SELECT * FROM ? WHERE name = ?", level, input(f"Input the {level} you want to {prompt}: "))
+    elif level == "category":
+        selected = db.execute("SELECT * FROM ? WHERE language_id = ? AND name = ?", level, reference, input(f"Input the {level} you want to {prompt}: "))
+    else:
+        selected = db.execute("SELECT * FROM ? WHERE category_id = ? AND name = ?", level, reference, input(f"Input the {level} you want to {prompt}: "))
 
-def fetch_id(level, prompt="access"):
-    selected = db.execute("SELECT * FROM ? WHERE name = ?", level, input(f"Input the {level} you want to {prompt}: "))
     if len(selected) == 0:
-        print(f"Such {level} does not exist in the database!")
+        print(f"Such {level} does not exist!")
         return -1
     return selected[0]["id"]
+
+def print_about():
+    print("This program helps expand your vocabulary when learning a new language")
+    print("It provides you three levels of: language, category and word")
+    print("That`s right! you can store words in different languages and have them categorized!")
+    print("The name of this project is 'Keasal', a Kurdish word meaning 'turtle',")
+    print("which is a symbol of wisdom and patience. May you enjoy your journey ;)")
 
 def main():
     reference_id = 0
     command = "0"
-    # Enter a non-digit input to exit the program
-    while command.isdigit():
+    while command != "exit":
         reference_id = guide(command, reference_id)
-        print("______________________")
+        print("________________________")
         command = input("command: ")
         print()
 

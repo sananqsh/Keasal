3bc7f07be3f4ca48f598b345c1550ae602a51bd7
Sanan <74050360+sananqsh@users.noreply.github.com>
2021-11-04 00:25:13 +0330

    organized prompts in an individual func

--- keasal.py
+++ keasal.py
@@ -13,93 +13,108 @@ def keasalpy(cmd, ref_id):
         print("Bad command!")
         return ref_id
 
-    print(f"cmd: {cmd} || ref_id: {ref_id}")
+    # print(f"cmd: {cmd} || ref_id: {ref_id}")
 
     if cmd == "0":
         position = 0
-        next_lvl = positions[position+1]
-        print(f"1.Manage in {next_lvl} level")  # TODO: add this line to guide()
-        print("2.About")                        # TODO: add this line to guide()
+
+        guide()
+        
         return 0
-    else:
-        cur_lvl = positions[position]
-        if position in range(0, 3) and cmd == "1":
-            if 0 < position:
-                target_element = fetch_element(ref_id)
-                if target_element[0] == False:    # Exception
-                    print(f"Such {cur_lvl} does not exist!")
-                    return ref_id
-                ref_id = target_element[0]["id"]
 
-            position +=1
-            cur_lvl = positions[position]
-
-            if position in range(1, 4):
-                print_elements(ref_id)
-
-            # TODO: put next 16 lines in a function named "guide"
-            print("0.Go to main")
-            if position in {1, 2}:
-                next_lvl = positions[position+1]
-                print(f"1.Manage in {next_lvl} level")
-
-            print(f"2.Add new {cur_lvl}")
-            print(f"3.Edit {cur_lvl}")
-            print(f"4.Remove {cur_lvl}")
-
-            #NEW
-            if position == 2:
-                print(f"5.Show all words in this {cur_lvl}")
-            if position in {2, 3}:
-                print(f"6.Take a peek at words")
-                print(f"7.Take test")
-            return ref_id
-            #####################################################
-
-        if cmd == "2":
-            if position == 0:
-                print_about()
-                return ref_id
-
-            new_element = fetch_element(ref_id, "add")
-            if new_element[0] != False:
-                print("This element already exists!")
-                return ref_id
-            add(ref_id, new_element[1])
-
-        elif 0 < position and (cmd == "3" or cmd == "4"):
-            target_element = fetch_element(ref_id, "edit or remove")
+    cur_lvl = positions[position]       
+    
+    if cmd == "1":
+        if 0 < position:
+            target_element = fetch_element(ref_id)
             if target_element[0] == False:    # Exception
                 print(f"Such {cur_lvl} does not exist!")
                 return ref_id
-            element_id = target_element[0]["id"]
+            ref_id = target_element[0]["id"]
 
-            if cmd == "3":
-                edit(element_id)
-            if cmd == "4":
-                remove(element_id)
+        position +=1
+        cur_lvl = positions[position]   # Is this line necessary?
+        
+        if position in range(1, 4):
+            print_elements(ref_id)
 
-        elif position in {2, 3} and  cmd in {"5", "6", "7"} :
-            if cmd in {"5", "6"}:
-                represent_lang_words(cmd, ref_id)
-            else:
-                print("TODO: test!")
-                # take_test(, )
-
-        else:
-            print("Incorrect command!")
+        guide()
+    
+    elif cmd == "2":
+        if position == 0:
+            print_about()
             return ref_id
 
-        # print(f"cmd: {cmd}; position: {position}")
-        if position != 0 and int(cmd) in range(0, 5):
-            print_elements(ref_id)
+        new_element = fetch_element(ref_id, "add")
+        if new_element[0] != False:
+            print("This element already exists!")
+            return ref_id
+        add(ref_id, new_element[1])
+
+    elif cmd in {"3", "4"}:
+        target_element = fetch_element(ref_id, "edit or remove")
+        if target_element[0] == False:    # Exception
+            print(f"Such {cur_lvl} does not exist!")
+            return ref_id
+        element_id = target_element[0]["id"]
+
+        if cmd == "3":
+            edit(element_id)
+        if cmd == "4":
+            remove(element_id)
+
+    elif cmd in {"5", "6", "7"} :
+        if cmd in {"5", "6"}:
+            represent_lang_words(cmd, ref_id)
+        else:
+            print("TODO: test!")
 
         return ref_id
 
-def is_valid_command(cmd):
+    else:
+        print("Incorrect command!")
+        return ref_id
+
+    # print(f"cmd: {cmd}; position: {position}")
+    if cmd in {"2", "3", "4"}:
+        print_elements(ref_id)
+
+    return ref_id
+
+
+
+def guide():
+    level = positions[position]
+
+    # 0-command also works anywhere but it`s more user friendly not to
+    # prompt it in position=0
+    if 0 < position:
+        print("0.Go to main")
+
+    if position < 3:
+        next_level = positions[position+1]
+        print(f"1.Manage in {next_level} level")
+
+    if position == 0:
+        print("2.About") 
+    else:
+        print(f"2.Add new {level}")
+        print(f"3.Edit {level}")
+        print(f"4.Remove {level}")
+
+    if position == 2:
+        print(f"5.Show all words in this {level}") 
+
+    if 1 < position: 
+        print(f"6.Take a peek at words")
+        print(f"7.Take test")
+
+    print("exit.To stop the program")
+
+def is_valid_command(cmd):    
     if cmd in {"0", "2"}:
         return True
-
+    
     if cmd == "1":
         if position in range(0, 3):
             return True
@@ -122,7 +137,7 @@ def is_valid_command(cmd):
         if position in {2, 3}:
             return True
         else:
-            return False
+            return False            
 
     else:
         return False
@@ -131,7 +146,7 @@ def represent_lang_words(cmd, reference):
     level = positions[position]
     # level is either category or word
     if level == "category":
-        words = db.execute("SELECT * FROM word WHERE category_id IN (SELECT id FROM category WHERE language_id=?)", reference)  #haven`t been tested
+        words = db.execute("SELECT * FROM word WHERE category_id IN (SELECT id FROM category WHERE language_id=?)", reference)
         for word in words:
             print(word["name"], end=": ")
             if cmd == "6":
@@ -174,7 +189,7 @@ def edit(element_id):
 
 def remove(element_id):
     level = positions[position]
-    db.execute("DELETE FROM ? where id = ?;", level, element_id)
+    db.execute("DELETE FROM ? where id = ?;", level, element_id)    
 
 def print_elements(reference):
     level = positions[position]
@@ -195,7 +210,7 @@ def print_elements(reference):
             print(el["name"])
 
     print("}")
-
+    
 def fetch_element(reference, prompt="access"):
     level = positions[position]
     target = input(f"Input the name of the {level} you want to {prompt}: ")
@@ -207,8 +222,8 @@ def fetch_element(reference, prompt="access"):
         # print(f"reference: {reference}")
         # print(target)
         selected = db.execute("SELECT * FROM category WHERE language_id = ? AND name = ?", reference, target)
-
-
+        
+    
     if len(selected) == 0:
         # such element does not exist
         return [False, target]
@@ -222,8 +237,7 @@ def print_about():
     print("The name of this project is 'Keasal', a Kurdish word meaning 'turtle',")
     print("which is a symbol of wisdom and patience. May you enjoy your journey ;)")
     print("     !)Pay attention to the prompts and follow them")
-    print("     0.Go to main")
-    print("  exit.To stop the program")
+   
 
 def main():
     reference_id = 0
